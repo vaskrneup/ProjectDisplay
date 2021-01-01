df7ba0fa1554a377dffbe964ffc71d45854c6758
vaskrneup <bhaskar.neupane.58@gmail.com>
2021-01-01 13:20:12 +0545

    Initial Test Table COmpleted !!

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-import {projectAsList} from "./project/ProjectConstants.js";
-import {renderTableBody} from "./render/table.js";
-
-document.getElementById("project-table").innerHTML = renderTableBody({
-    tableGrid: projectAsList
-});
-console.log(renderTableBody({
-    tableGrid: projectAsList
-}))
+// document.getElementById("project-table").innerHTML = "";
